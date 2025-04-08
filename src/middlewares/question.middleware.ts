61c7ffece4e2eed9e507a59a19dc1deb7e2fe0d1
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2025-04-08 22:27:31 +0700

    fix: question middleware

--- src/middlewares/question.middleware.ts
+++ src/middlewares/question.middleware.ts
@@ -17,18 +17,18 @@ export const questionValidator = validate(
         errorMessage: MESSAGES.VALIDATION_MESSAGES.QUESTIONS.QUESTION_TEXT.LENGTH_MUST_BE_FROM_3_TO_1000
       }
     },
-    paragraph_id: {
-      isString: {
-        errorMessage: MESSAGES.VALIDATION_MESSAGES.QUESTIONS.PARAGRAPH_ID.MUST_BE_A_STRING
-      },
-      custom: {
-        options: async (value) => {
-          if (validateObjectId(value, MESSAGES.VALIDATION_MESSAGES.QUESTIONS.PARAGRAPH_ID.MUST_BE_VALID_OBJECT_ID)) {
-            return true
-          }
-        }
-      }
-    },
+    // paragraph_id: {
+    //   isString: {
+    //     errorMessage: MESSAGES.VALIDATION_MESSAGES.QUESTIONS.PARAGRAPH_ID.MUST_BE_A_STRING
+    //   },
+    //   custom: {
+    //     options: async (value) => {
+    //       if (validateObjectId(value, MESSAGES.VALIDATION_MESSAGES.QUESTIONS.PARAGRAPH_ID.MUST_BE_VALID_OBJECT_ID)) {
+    //         return true
+    //       }
+    //     }
+    //   }
+    // },
     category_id: {
       isString: {
         errorMessage: MESSAGES.VALIDATION_MESSAGES.QUESTIONS.CATEGORY_ID.MUST_BE_A_STRING
